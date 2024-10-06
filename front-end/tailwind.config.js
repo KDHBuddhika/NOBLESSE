b2113fd5c0951cb945ccc22a681c7ed197700a4e
Prasad Prasanna <prasadkvithana@gmail.com>
2024-10-06 23:47:46 +0530

    Fixed Merging Conflicts

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,36 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-<<<<<<< HEAD
-  content: [ "./src/**/*.{js,jsx,ts,tsx}",],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
-=======
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    screens: {
-      sm: "340px",
-      md: "540px",
-      lg: "768px",
-      xl: "1180px"
-    },
-    extend: {},
-    fontFamily: {
-      Lobster: ["Lobster", "sans-serif"],
-      Roboto: ["Roboto", "sans-serif"]
-    },
-    container:{
-      center: true,
-      padding: {
-        DEFAULT: "12px",
-        md: "32px"
-      }
-    }
-
-  },
-  plugins: [],
-}
->>>>>>> main-prasad
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		screens: {
+			sm: "340px",
+			md: "540px",
+			lg: "768px",
+			xl: "1180px",
+		},
+		extend: {},
+		fontFamily: {
+			Lobster: ["Lobster", "sans-serif"],
+			Roboto: ["Roboto", "sans-serif"],
+		},
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: "12px",
+				md: "32px",
+			},
+		},
+	},
+	plugins: [],
+};
