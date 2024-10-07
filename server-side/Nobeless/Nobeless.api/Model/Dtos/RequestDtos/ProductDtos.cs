6642fadf9a8a0afa8f1311750317eaf6b59e2170
KDHBuddhika <136336705+KDHBuddhika@users.noreply.github.com>
2024-10-07 22:34:22 +0530

    product add update

--- server-side/Nobeless/Nobeless.api/Model/Dtos/RequestDtos/ProductDtos.cs
+++ server-side/Nobeless/Nobeless.api/Model/Dtos/RequestDtos/ProductDtos.cs
@@ -5,5 +5,13 @@
 
         public string Name { get; set; }
         public string Description { get; set; }
+
+        public double StartingPrise { get; set; }
+
+        public IFormFile thumbnailImage { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public int CategoryId { get; set; }
     }
 }
