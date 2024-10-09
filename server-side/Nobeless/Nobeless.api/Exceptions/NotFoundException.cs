191816ef35e018055820bce096194eb8d1133097
KDHBuddhika <136336705+KDHBuddhika@users.noreply.github.com>
2024-10-09 16:27:49 +0530

    complete product

---
+++ server-side/Nobeless/Nobeless.api/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿namespace Nobeless.api.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException() : base() { }
+
+        public NotFoundException(string message) : base(message) { }
+
+        public NotFoundException(string message, IOException innerException) : base(message, innerException) { }
+
+    }
+}
