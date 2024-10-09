0101eed294cf2be92c4498ce5512eaf6133b890b
KDHBuddhika <136336705+KDHBuddhika@users.noreply.github.com>
2024-10-10 01:00:24 +0530

    add front

---
+++ front-end/src/setupTests.js
@@ -0,0 +1,19 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
+
+
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://localhost:7231', // Your .NET API URL
+      secure: false,
+      changeOrigin: true,
+    })
+  );
+};
---
+++ server-side/Nobeless/Nobeless.api/Util/AuctionStatusCheckerService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nobeless.api.Data;
+
+namespace Nobeless.api.Util
+{
+    public class AuctionStatusCheckerService : BackgroundService
+    {
+        private readonly ILogger<AuctionStatusCheckerService> _logger;
+        private readonly IServiceProvider _serviceProvider;
+
+        public AuctionStatusCheckerService(ILogger<AuctionStatusCheckerService> logger, IServiceProvider serviceProvider)
+        {
+            _logger = logger;
+            _serviceProvider = serviceProvider;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Auction Status Checker Service started.");
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await CheckAndUpdateAuctionStatusesAsync();
+                await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken); // Check every 1 minute
+            }
+        }
+
+        private async Task CheckAndUpdateAuctionStatusesAsync()
+        {
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<NobelessDbContext>(); // Inject your DbContext
+                var currentTime = DateTime.UtcNow;
+
+                var auctionsToUpdate = await dbContext.auctions
+                    .Where(a => a.EndTime <= currentTime && !a.IsCompleted)
+                    .ToListAsync();
+
+                foreach (var auction in auctionsToUpdate)
+                {
+                    auction.IsCompleted = true;
+                }
+
+                await dbContext.SaveChangesAsync();
+            }
+
+
+
+        }
+    }
+}
