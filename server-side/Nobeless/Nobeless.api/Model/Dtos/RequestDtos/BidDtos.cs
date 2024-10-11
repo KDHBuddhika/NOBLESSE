3f252a4ee214586487d4c7fb47b0e5e13ad8f850
KDHBuddhika <136336705+KDHBuddhika@users.noreply.github.com>
2024-10-11 23:42:16 +0530

    bid add

---
+++ server-side/Nobeless/Nobeless.api/Model/Dtos/RequestDtos/BidDtos.cs
@@ -0,0 +1,16 @@
+﻿using Nobeless.api.Model.Domain;
+
+namespace Nobeless.api.Model.Dtos.RequestDtos
+{
+    public class BidDtos
+    {
+        public decimal Amount { get; set; }
+        public DateTime BidTime { get; set; }
+        public string State { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public int AuctionId { get; set; }
+      
+    }
+}
---
+++ server-side/Nobeless/Nobeless.api/Service/IMPL/BidServiceIMPL.cs
@@ -0,0 +1,37 @@
+﻿using Nobeless.api.Data;
+using Nobeless.api.Model.Domain;
+using Nobeless.api.Model.Dtos.RequestDtos;
+
+namespace Nobeless.api.Service.IMPL
+{
+    public class BidServiceIMPL : BidService
+    {
+
+        private  readonly NobelessDbContext _DbContext;
+
+        public BidServiceIMPL(NobelessDbContext nobelessDbContext)
+        {
+             this._DbContext = nobelessDbContext;
+        }
+        public async Task AddBid(BidDtos bidDtos)
+        {
+            if (bidDtos == null)
+            {
+                throw new ArgumentNullException(nameof(bidDtos));
+            }
+
+            var bid = new Bid 
+            {
+                Amount = bidDtos.Amount ,
+                State = bidDtos.State ,
+                AuctionId = bidDtos.AuctionId ,
+                BidTime = bidDtos.BidTime ,
+                UserId = bidDtos.UserId 
+                
+            };
+
+            await _DbContext.Bids.AddAsync(bid);
+            await _DbContext.SaveChangesAsync();
+        }
+    }
+}
